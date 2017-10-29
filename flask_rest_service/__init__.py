f5505b619b01f211e62e2eff28933fafb8eebf37
Brian Tsai <brian.yj.tsai@utexas.edu>
2017-10-29 11:23:47 -0500

    Hello

--- flask_rest_service/__init__.py
+++ flask_rest_service/__init__.py
@@ -62,6 +62,8 @@ def hello():
             new_tweet+=tweet['user']['name'] + "%"
             new_tweet+=str(tweet['geo']['coordinates'][0])
             new_tweet+="%"
+            new_tweet+=str("@" + tweet['user']['name'])
+            new_tweet+="%"
             new_tweet+=str(tweet['geo']['coordinates'][1])
             new_tweet+="%"
         except:
